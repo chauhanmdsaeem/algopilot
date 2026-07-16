b31ad0c59564e6656b29daa6edbbccff19980d23
Md Saeem <saeemchauhan01@gmail.com>
2026-07-16 12:33:25 +0530

    Add User entity and repository

---
+++ backend/src/main/java/com/algopilot/backend/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.algopilot.backend.repository;
+
+import com.algopilot.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
